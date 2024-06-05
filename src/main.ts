a6662145ea8e962818d24d10749bdac903d14624
Minzyx <minzybtw@gmail.com>
2024-06-05 17:23:27 -0300

    feat: sobre

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,20 @@ import { Engine, FadeInOut } from "excalibur";
 import { welcomeScene } from "./scenes/welcomeScene";
 import { loader } from "./resources";
 import { historyScene } from "./scenes/historyScene";
+import { gamificationScene } from "./scenes/gamificationScene";
 
 const game = new Engine ({
   width: 1200,
   height: 800,
-  canvasElementId: "jogo"
+  canvasElementId: "jogo" 
 })
 
 game.addScene("bemvindo", new welcomeScene())
 game.addScene("historia", new historyScene())
+game.addScene("gamificacao", new gamificationScene())
 
 game.start(loader).then(() => {
-  game.goToScene("bemvindo",{
+  game.goToScene("historia",{
     sourceOut: new FadeInOut({duration: 1000})
   })
   
